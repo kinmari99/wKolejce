2cf917a2ed43684f3da4361a268dcc9134d14e08
kinmari99 <kinga.wojtanowicz@microsoft.wsei.edu.pl>
2026-05-30 15:35:07 +0200

    Making run_all_local more dynamic

--- pipelines/run_all_local.py
+++ pipelines/run_all_local.py
@@ -1,21 +1,17 @@
 import subprocess
 import sys
 from pathlib import Path
+from sqlalchemy import text
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+from db import get_engine
 
 
 PIPELINE_DIR = Path(__file__).resolve().parent
 
-STEPS = [
-    ["ingest_nfz_to_rds_local.py"],
-    ["transform_rds_to_dw_local.py", "11"],
-    ["publish_dw_to_ads_local.py", "11"],
-]
 
-
-def run_step(step: list[str]):
-    script_name = step[0]
-    args = step[1:]
-
+def run_script(script_name: str, *args: str):
     script_path = PIPELINE_DIR / script_name
 
     print(f"\n=== Running: {script_name} {' '.join(args)} ===")
@@ -31,11 +27,32 @@ def run_step(step: list[str]):
     print(f"=== Finished: {script_name} ===")
 
 
-def main():
-    print("Starting local pipeline...")
+def get_latest_success_batch_id() -> int:
+    engine = get_engine()
 
-    for step in STEPS:
-        run_step(step)
+    with engine.connect() as conn:
+        batch_id = conn.execute(text("""
+            select top 1 load_batch_id
+            from rds.load_batch
+            where source_system = 'NFZ_TERMINY_LECZENIA'
+              and status = 'SUCCESS'
+            order by load_batch_id desc
+        """)).scalar_one_or_none()
+
+    if batch_id is None:
+        raise ValueError("Nie znaleziono żadnego poprawnego batcha w rds.load_batch")
+
+    return int(batch_id)
+
+
+def main():
+    run_script("ingest_nfz_to_rds_local.py")
+
+    load_batch_id = get_latest_success_batch_id()
+    print(f"\nUsing load_batch_id = {load_batch_id}")
+
+    run_script("transform_rds_to_dw_local.py", str(load_batch_id))
+    run_script("publish_dw_to_ads_local.py", str(load_batch_id))
 
     print("\nAll local pipeline steps finished successfully.")
 
